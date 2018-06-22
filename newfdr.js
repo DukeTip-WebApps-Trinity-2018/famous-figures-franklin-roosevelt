00a49ed365f6a9e963c58fea0b6d973e46fcdd83
EmptyBowl320 <40175855+EmptyBowl320@users.noreply.github.com>
2018-06-22 15:52:42 -0500

    Add files via upload

---
+++ newfdr.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$('#title-text')
+})
+
+$("#title-text").hover(
+    function () {
+        $(this).append($("<span> ***</span>"));
+    }, function () {
+        $(this).find("span:last").remove();
+    }
+);
+
+$("li.fade").hover(function () {
+    $(this).fadeOut(100);
+    $(this).fadeIn(500);
+    }
+);
+
+$("")
